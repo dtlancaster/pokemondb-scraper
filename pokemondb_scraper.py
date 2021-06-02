1bf93e15b63c6a5f45f134cea00514d1567f4ca1
dtlancaster <Tuscarora007@icloud.com>
2021-06-02 15:37:27 -0400

    began work on training data, changed string formatting, fix printing bug later.

--- pokemondb_scraper.py
+++ pokemondb_scraper.py
@@ -7,7 +7,6 @@ import urllib.robotparser
 headers = {'user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'}
 base_url = 'https://pokemondb.net/pokedex'
 robot_url = 'https://pokemondb.net/robots.txt'
-pokemon = input("Which Pokémon would you like information about? Enter its name here: ")
 data = []
 
 
@@ -35,12 +34,12 @@ def get_page(url):
         return None
 
 """
-Function to parse HTML of requested page and append attributes to a list.
+Function to parse HTML of requested page and print basic attributes.
 """
-def get_attributes(name):
+def get_basic_attributes(name):
     n = get_page(base_url + "/" + name)
     name = n.select_one("h1").text
-    data_table = n.find("table", class_="vitals-table")
+    data_table = n.find_all("table", class_="vitals-table")[0]
     nat_dex_num = data_table('td')[0].text
     poke_type = data_table.find_all("a", class_=re.compile('type-icon'))
     species = data_table('td')[2].text
@@ -49,21 +48,38 @@ def get_attributes(name):
     abilities = data_table('td')[5].find_all("a")
     print("\nBASIC DATA\n")
     print("Pokémon Name: " + name)
-    print("National Pokédex Number: " + nat_dex_num)
+    print("National Pokédex Number: {}".format(nat_dex_num))
     if len(poke_type) > 1:
-        print("Primary Type: " + poke_type[0].get_text())
-        print("Secondary Type: " + poke_type[1].get_text())
+        print("Primary Type: ", poke_type[0].get_text())
+        print("Secondary Type: ", poke_type[1].get_text())
     else:
-        print("Type: " + poke_type[0].get_text())
-    print("Species: " + species)
-    print("Height: " + height)
-    print("Weight: " + weight)
+        print("Type: ", poke_type[0].get_text())
+    print("Species: ", species)
+    print("Height: ", height)
+    print("Weight: ", weight)
     for ability in abilities:
         if len(abilities) == 1:
-            print("Ability: " + ability.get_text())
+            print("Ability: ", ability.get_text())
         elif abilities.index(ability) != (len(abilities) - 1):
-            print("Ability: " + ability.get_text())
+            print("Ability: ", ability.get_text())
         else:
-            print("Hidden Ability: " + ability.get_text())
+            print("Hidden Ability: ", ability.get_text())
 
-get_attributes(pokemon)
+"""
+Function to parse HTML of requested page and print training attributes.
+"""
+def get_training_attributes(name):
+    n = get_page(base_url + "/" + name)
+    data_table = n.find_all("table", class_="vitals-table")[1]
+    ev_yield = data_table('td')[0].text
+    catch_rate = data_table('td')[1].text
+    base_friendship = data_table('td')[2].text
+    print("\nTRAINING DATA\n")
+    print("EV Yield: {}".format(ev_yield))
+    print("Catch Rate: {}".format(catch_rate))
+    print("Base Friendship: " + base_friendship)
+
+
+pokemon = input("Which Pokémon would you like information about? Enter its name here: ")
+get_basic_attributes(pokemon)
+get_training_attributes(pokemon)
